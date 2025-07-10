82fee4ed758dbdc0f9f3f1fc866f5dfbec1f892d
AestheticAaditya0203 <103627563+AestheticAaditya0203@users.noreply.github.com>
2025-07-10 09:40:37 +0530

    Update navbar.js

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
                     Aaditya Joshi
                 </Link>
             </div>
-            <a href="https://addy-web.s3.ap-south-1.amazonaws.com/AadityaJoshi_Resume.pdf" target="_blank" className="cta-btn" >Resume</a>
+            <a href="https://krxjab57hs2ywbhr.public.blob.vercel-storage.com/AadityaJoshi_Resume-myTCaNEyxKyD2ijZtfUlDISRnhc6HI.pdf" target="_blank" className="cta-btn" >Resume</a>
         </div>
     )
 }
